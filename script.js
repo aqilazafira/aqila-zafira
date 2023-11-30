d88b8cd3e73bff22ff08c26fbd11f6cf0d05e10e
aqilazafira <149031564+aqilazafira@users.noreply.github.com>
2023-11-30 15:10:38 +0700

    aqila

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 document.getElementById('edukasi').style.display="none";    document.getElementById("btn").addEventListener("click", () => document.getElementById('edukasi').style.display="");
+const d = new Date("2015-03-25");
+      document.getElementById("demo").innerHTML = d;
